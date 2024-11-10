f43d052e1bc450b5152cb5082ef90561f51bb609
rsanch43 <rsanch43@email.cpcc.edu>
2024-11-09 23:57:32 -0500

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -25,7 +25,17 @@ public class Policy {
         weight = 0.0;
     }
 
-    // Constructor with parameters
+    /**
+      Constructor with parameters
+      @param policyNum The policy number for the policy.
+      @param provider The provider name for the insurance policy.
+      @param first The first name of the policyholder.
+      @param last The last name of the policyholder.
+      @param a The age of the policyholder.
+      @param status The smoking status of the policyholder
+      @param h The height of the policyholder in inches.
+      @param w The weight of the policyholder in pounds.
+   */
     public Policy(String policyNum, String provider, String first, String last, int a,
                   String status, double h, double w) {
         policyNumber = policyNum;
@@ -38,78 +48,146 @@ public class Policy {
         weight = w;
     }
 
-    //Setters: Stores the value
+    /**
+      Sets policy number for the insurance policy.
+      @param policyNum The policy number to be set.
+    */
     public void setPolicyNumber(String policyNum) {
          policyNumber = policyNum;
     }
     
+    /**
+      Sets provider name for the insurance policy.
+      @param provider The provider name to be set.
+    */
     public void setProviderName(String provider) {
         providerName = provider;
     }
     
+    /**
+      Sets first name of the policyholder
+      @param first The fisrt name of policyholder.
+    */
     public void setFirstName(String first) {
         firstName = first;
     }
     
+    /**
+      Sets last name of the policyholder
+      @param last The last name of policyholder
+   */
     public void setLastName(String last) {
         lastName = last;
     }
-
+    
+    /**
+      Sets age of the policyholder.
+      @param a The age of policyholder.
+   */
     public void setAge(int a) {
         age = a;
     }
     
+    /**
+      Sets smoking status of policyholder
+      @param status The smoking status of policyholder
+   */
     public void setSmokingStatus(String status) {
         smokingStatus = status;
     }
     
+    /**
+    Sets height of policyholder
+    @param h The height of policyholder.
+   */
     public void setHeight(double h) {
         height = h;
     }
     
+    /**
+      Sets weight of policyholder
+      @param w The weight of policyholder.
+   */
     public void setWeight(double w) {
         weight = w;
     }
     
-    // Getters: Returns
+    /**
+      Gets policy number of insurance policy
+      @return The policy number.
+   */
     public String getPolicyNumber() {
         return policyNumber;
     }
 
+    /**
+       Gets provider name of insurance policy
+       @return The provider name.
+   */
     public String getProviderName() {
         return providerName;
     }
-
+    
+    /**
+      Gets policyholder first name
+      @return The first name of policyholder.
+   */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+      Gets policyholder last name
+      @return The last name of policyholder.
+   */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+      Gets policyholder age
+      @return The age of policyholder.
+   */
     public int getAge() {
         return age;
     }
 
+    /**
+      Gets policyholder smoking staus
+      @return The smoking status of policyholder.
+   */
     public String getSmokingStatus() {
         return smokingStatus;
     }
 
+    /**
+      Gets policyholder height
+      @return The height of the policyholder.
+   */
     public double getHeight() {
         return height;
     }
 
+    /**
+      Gets policyholder weight
+      @return The weight of the policyholder.
+   */
     public double getWeight() {
         return weight;
     }
 
-    // Method to calculate BMI
+    /**
+      Calculates the BMI for the policyholder
+      @return The BMI of policyholder.
+   */
     public double calculateBMI() {
         return (weight * 703) / (height * height);
     }
 
-    // Method to calculate policy price
+    /**
+      Calculates the price of the insurance policy.
+      @return The total price of the insurance policy.
+   */
     public double calculatePolicyPrice() {
         double price = 600; // base fee
 
