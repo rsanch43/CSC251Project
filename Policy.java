7640aaa6659dc7993b509f4d82265dc44b9d9945
rsanch43 <rsanch43@email.cpcc.edu>
2024-11-25 18:31:26 -0500

    Update Policy.java
    Moved fields and methods to the PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -2,40 +2,27 @@
    Policy class
 */
 
-public class Policy {
-    // Attributes
-    private String policyNumber;
-    private String providerName;
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String smokingStatus; // "smoker" or "non-smoker"
-    private double height; // in inches
-    private double weight; // in pounds
+public class Policy 
+{
+   // Attributes
+   private String policyNumber;
+   private String providerName;
+   private PolicyHolder holder;
 
-    // No-argument constructor
-    public Policy() {
-        policyNumber = "";
-        providerName = "";
-        firstName = "";
-        lastName = "";
-        age = 0;
-        smokingStatus = "non-smoker";
-        height = 0.0;
-        weight = 0.0;
+   // No-argument constructor
+   public Policy() 
+   {
+      policyNumber = "";
+      providerName = "";
+      holder = new PolicyHolder();
     }
 
     // Constructor with parameters
-    public Policy(String policyNum, String provider, String first, String last, int a,
-                  String status, double h, double w) {
+    public Policy(String policyNum, String provider, PolicyHolder holder) 
+    {
         policyNumber = policyNum;
         providerName = provider;
-        firstName = first;
-        lastName = last;
-        age = a;
-        smokingStatus = status;
-        height = h;
-        weight = w;
+        this.holder = new PolicyHolder(holder);
     }
 
     //Setters: Stores the value
@@ -47,29 +34,10 @@ public class Policy {
         providerName = provider;
     }
     
-    public void setFirstName(String first) {
-        firstName = first;
-    }
-    
-    public void setLastName(String last) {
-        lastName = last;
-    }
-
-    public void setAge(int a) {
-        age = a;
-    }
-    
-    public void setSmokingStatus(String status) {
-        smokingStatus = status;
-    }
-    
-    public void setHeight(double h) {
-        height = h;
-    }
-    
-    public void setWeight(double w) {
-        weight = w;
-    }
+    public void setHolder(PolicyHolder holder)
+    {
+      this.holder = new PolicyHolder(holder);
+   }
     
     // Getters: Returns
     public String getPolicyNumber() {
@@ -79,52 +47,35 @@ public class Policy {
     public String getProviderName() {
         return providerName;
     }
+    
+    public PolicyHolder getHolder()
+    {
+      return holder;
+   }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getSmokingStatus() {
-        return smokingStatus;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    // Method to calculate BMI
-    public double calculateBMI() {
-        return (weight * 703) / (height * height);
-    }
-
-    // Method to calculate policy price
-    public double calculatePolicyPrice() {
-        double price = 600; // base fee
-
-        if (age > 50) {
-            price += 75; // additional fee for age
-        }
-        if (smokingStatus.equalsIgnoreCase("smoker")) {
-            price += 100; // additional fee for smokers
-        }
-
-        double bmi = calculateBMI();
-        if (bmi > 35) {
-            price += (bmi - 35) * 20; // additional fee based on BMI
-        }
-
-        return price;
-    }
+   // Method to calculate policy price
+   public double calculatePolicyPrice() 
+   {
+      double price = 600; // base fee
+      
+      if (holder.getAge() > 50) 
+      {
+         price += 75; // additional fee for age
+      }
+      
+      if (holder.getSmokingStatus().equalsIgnoreCase("smoker")) 
+      {
+         price += 100; // additional fee for smokers
+      }
+      
+      double bmi = holder.calculateBMI();
+      
+      if (bmi > 35) 
+      {
+         price += (bmi - 35) * 20; // additional fee based on BMI
+      }
+      
+      return price;
+   }
+   
 }
